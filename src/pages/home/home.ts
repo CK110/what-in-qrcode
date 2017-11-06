984d1956bb10a1231251319088fc9af4ab2ce03d
CK <chenkai4dev@163.com>
2017-11-06 17:46:30 +0800

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {BarcodeScanner} from "@ionic-native/barcode-scanner";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  code:string='';
+  constructor(public navCtrl: NavController,private barcodeScanner: BarcodeScanner) {
+
+  }
+  qrcode(){
+    this.barcodeScanner.scan().then((barcodeData) => {
+      if (barcodeData.cancelled) {
+        console.log("用户取消操作");
+        return false;
+      }
+      this.code = barcodeData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+}
